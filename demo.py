7cf4baaf87e23cf107753df73e9be4771d9118dd
Tomasz Rojek <tomek@rojek.cc>
2020-10-11 11:58:20 +0200

    add poetry

--- demo.py
+++ demo.py
@@ -9,13 +9,14 @@ from StockScraper.Clients.BankierClient.bankier_client import BankierClient
 from StockScraper.Generic.configuration import get_data_from_config
 from StockScraper.Generic.clients import get_client
 
+"""
 try:
     SCRAPER_HOME = os.environ['SCRAPER_HOME']
     logging.info('SCRAPER_HOME =  %s', SCRAPER_HOME)
 except KeyError:
     err = 'Environment variable SCRAPER_HOME not set'
     raise KeyError(err)
-
+"""
 
 def main() -> None:
     """Presentation of created scrapper.
@@ -25,7 +26,7 @@ def main() -> None:
     logging.basicConfig(filename='StockScraper.log')
 
     config_parser = configparser.ConfigParser()
-    config_parser.read(SCRAPER_HOME + '/project.cfg')
+    config_parser.read('./project.cfg')
 
     path_to_config = Path(config_parser['DEFAULT']['configs_path']) / 'config.json'
     client_config = get_data_from_config(str(path_to_config))
@@ -37,7 +38,7 @@ def main() -> None:
             for company_symbol in client_config[host]:
                 logging.info('Getting data for {}'.format(company_symbol))
                 data = client.get_market_data(company_symbol=company_symbol, days_back=10)
-
+                print(data)
 
 if __name__ == '__main__':
     main()
